0ad06b0187943ea7eb56722eedba91ec322bb34f
saranya-1621 <51949644+saranya-1621@users.noreply.github.com>
2019-06-26 15:37:01 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,5 @@
 ch=input("please enter your own character:")
 if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-    print("the given character",ch,"is a alphabet")
+    print("the given character",ch,"is a Alphabet")
 else:
-     print("the given character",ch,"is not a alphabet")
+     print("the given character",ch,"is Not alphabet")
